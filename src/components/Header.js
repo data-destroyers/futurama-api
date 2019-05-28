8b5b6266c3ccc41b742ecb3e93a1fc93dca0fcb1
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-28 13:27:27 -0700

    made header component and exported to app

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import Component from './Component.js';
+
+class Header extends Component {
+    renderTemplate() {
+        return /*html*/`
+        <h1>Futurama</h1>
+        `;
+    }
+}
+
+export default Header;
