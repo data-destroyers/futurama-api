f3698b4bd014843fb9694d196250c05c11906df4
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-28 15:24:26 -0700

    Imported loading component and changed to false upon data being recieved

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import Component from './Component.js';
 import Header from './Header.js';
 import futuramaApi from '../services/futurama-api.js';
 import CharacterList from './CharacterList.js';
-
+import Loading from './Loading.js';
 
 class App extends Component { 
 
@@ -16,24 +16,28 @@ class App extends Component {
         const characterList = new CharacterList({ characters: [] });
         main.appendChild(characterList.render());
 
+        const loading = new Loading({ loading: true });
+        main.appendChild(loading.render());
+
         futuramaApi.getCharacters()
             .then(characters => {
-                characterList.update({ characters });
+                characterList.update({ characters }); 
+            })
+            .finally(() => {
+                loading.update({ loading: false });
             });
 
         return dom;
     }
 
     renderTemplate() {
-        return /*html*/ `
+        return /*html*/`
         <div>
-
             <main>
-
             </main>
         </div>
         `;
-    }   
+    }
 }
 
 export default App;
