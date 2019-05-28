978c387fbc5efc7c87e5430f4f18af48811f359e
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-28 15:10:45 -0700

    updated character list to use real api data

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import Component from './Component.js';
 import Header from './Header.js';
 import futuramaApi from '../services/futurama-api.js';
 import CharacterList from './CharacterList.js';
-import characters from '../../test/characters.js';
+
 
 class App extends Component { 
 
@@ -13,11 +13,13 @@ class App extends Component {
         const header = new Header();
         dom.insertBefore(header.render(), main);
 
-        const characterList = new CharacterList({ characters });
+        const characterList = new CharacterList({ characters: [] });
         main.appendChild(characterList.render());
 
         futuramaApi.getCharacters()
-            .then(characters => console.log(characters));
+            .then(characters => {
+                characterList.update({ characters });
+            });
 
         return dom;
     }
