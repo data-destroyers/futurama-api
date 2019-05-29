e1ece767935fded7818050f90498f209f7863707
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-29 11:56:51 -0700

    Imported searchParams into API and updated query URL

--- src/services/futurama-api.js
+++ src/services/futurama-api.js
@@ -1,8 +1,17 @@
 const URL = 'http://futuramaapi.herokuapp.com/api/quotes';
 
 const futuramaApi = {
-    getCharacters() {
-        return fetch(URL)
+    getCharacters(search) {
+        let url = URL;
+
+        if(search) {
+            const searchParams = new URLSearchParams();
+            searchParams.set('search', search);
+            const query = searchParams.toString();
+            url = `${URL}?${query}`;
+        }
+
+        return fetch(url)
             .then(response => response.json());
     }
 };
