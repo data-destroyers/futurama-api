adbbbdd3b9604cfe5b423399e7d8fb7cff3c259d
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-29 11:31:00 -0700

    added search component to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
 import Component from './Component.js';
+import Search from './Search.js';
 
 class Header extends Component {
+    render() {
+        const header = this.renderDOM();
+
+        const search = new Search();
+        header.appendChild(search.render());
+
+        return header;
+    }
     renderTemplate() {
         return /*html*/`
-            <h1>Futurama</h1>
+            <header>
+                <h1>Futurama</h1>
+            </header>
         `;
     }
 }
