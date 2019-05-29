a110a567281b825fefb45b83c1e9dfe46c9a5faa
Dan Hogan <hogan.daniel@hotmail.com>
2019-05-29 11:57:24 -0700

    Return empty div if not loading

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,14 @@
 import Component from './Component.js';
 
 class Loading extends Component {
+
     renderTemplate() {
+        const loading = this.props.loading;
+
+        if(!loading) {
+            return '<div></div>';
+        }
+
         return /*html*/`
             <div class="loading">
                 <img class="loading-gif" src="../../assets/loading.gif">
